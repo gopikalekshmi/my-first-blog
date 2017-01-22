1603b35da79009461d470da700f5249d5046b4b6
gopikajs <gopika@gopika.com>
2017-01-22 15:19:18 +0530

    django,first commit

---
+++ basic.py
@@ -0,0 +1,17 @@
+def hi():
+  print("django")
+hi()
+if 2>2:
+  print("gopika")
+else:
+  print("haris")
+def hello(name):
+  print("hai "+  name)
+hello("lbs")
+girls=['gopika','arya']
+for name in girls:
+  hello(name)
+
+
+
+
